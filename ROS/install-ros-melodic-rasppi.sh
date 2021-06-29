6b47241350984ed3dbd03d35ef60c0d71b47d3e0
Kazuhiro Ouchi <kanpapa@kanpapa.com>
2021-06-30 08:13:57 +0900

    Added install ROS script for Raspberry Pi

---
+++ ROS/install-ros-melodic-rasppi.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -e
+
+# Reference sites
+# https://demura.net/education/18077.html
+
+echo "[Add the ROS repository]"
+if [ ! -e /etc/apt/sources.list.d/ros-latest.list ]; then
+    sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+fi
+
+echo "[Update the package]"
+sudo apt-get update
+sudo apt install -y curl
+curl -sSL 'http://keyserver.ubuntu.com/pks/lookup?op=get&search=0xC1CF6E31E6BADE8868B172B4F42ED6FBAB17C654' | sudo apt-key add -
+
+sudo apt update
+sudo apt upgrade -y
+
+echo "[Installing ROS and ROS Packages]"
+sudo apt install -y ros-melodic-desktop-full
+
+echo "[Install rosdep and initialize.]"
+sudo apt install -y python-rosdep
+sudo rosdep init
+rosdep update
+
+echo "[Setup bash.]"
+cd
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+echo "[Install tools that make installation easier.]"
+sudo apt install -y python-rosinstall python-rosinstall-generator python-wstool build-essential
